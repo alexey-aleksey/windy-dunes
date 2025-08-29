b3206620cdbdce5b26287a033fb63a0221ae061c
aleksey_tkachenko@hotmail.com <aleksey_tkachenko@hotmail.com>
2025-08-29 02:12:03 +0000

    Edited test_files

--- server_code/test_files.py
+++ server_code/test_files.py
@@ -24,14 +24,16 @@ from stanza.pipeline.core import DownloadMethod
 #   return 42
 #
 
-files = glob('_/theme/phrases/*.aac')
-files.extend(glob('_/theme/phrases/*.m4a'))
-nlp = stanza.Pipeline('uk', download_method=DownloadMethod.REUSE_RESOURCES)
-
-for file in files:
-  str = Path(file).stem
-  doc_name = str + ".json"
-  doc = nlp(str)
-  dict = doc.to_dict()
-  with codecs.open(doc_name, 'w', encoding='utf-8') as f:
-    json.dump(dict, f, ensure_ascii=False)
+@anvil.server.callable
+def stanza_phrases ():
+  files = glob('_/theme/phrases/*.aac')
+  files.extend(glob('_/theme/phrases/*.m4a'))
+  nlp = stanza.Pipeline('uk', download_method=DownloadMethod.REUSE_RESOURCES)
+  
+  for file in files:
+    str = Path(file).stem
+    doc_name = str + ".json"
+    doc = nlp(str)
+    dict = doc.to_dict()
+    with codecs.open(doc_name, 'w', encoding='utf-8') as f:
+      json.dump(dict, f, ensure_ascii=False)
