688838496887b8f8725122943e665644f64b5c65
aleksey_tkachenko@hotmail.com <aleksey_tkachenko@hotmail.com>
2025-09-10 02:06:25 +0000

    Edited test_files, control_panel, main_form and settings

--- client_code/control_panel/__init__.py
+++ client_code/control_panel/__init__.py
@@ -1,5 +1,11 @@
 from ._anvil_designer import control_panelTemplate
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
 import anvil.server
 
 
@@ -9,3 +15,4 @@ class control_panel(control_panelTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    anvil.users.login_with_form()
