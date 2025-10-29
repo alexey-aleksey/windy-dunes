3706096b66410e07d2448cea87a88d938ccbc73d
aleksey_tkachenko@hotmail.com <aleksey_tkachenko@hotmail.com>
2025-10-29 03:16:23 +0000

    Created admin_form; Edited control_panel

--- client_code/admin_form/__init__.py
+++ client_code/admin_form/__init__.py
@@ -1,26 +1,17 @@
-from ._anvil_designer import control_panelTemplate
+from ._anvil_designer import admin_formTemplate
 from anvil import *
+import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.users
-import anvil.server
 
 
-class control_panel(control_panelTemplate):
+class admin_form(admin_formTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-    anvil.users.login_with_form()
-
-  def button_admin_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    user = anvil.users.get_user()
-
-    if user['role'] == 'admin':
-     open_form('admin')
-    pass
